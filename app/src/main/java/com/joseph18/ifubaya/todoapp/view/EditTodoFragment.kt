97bd2ea9ef578daf8833d0536dd376c03f71f0ca
Joseph Kristiano <o.joseph.k@gmail.com>
2021-10-28 15:59:10 +0700

    W09 - View: Fragment Edit Todo init

---
+++ app/src/main/java/com/joseph18/ifubaya/todoapp/view/EditTodoFragment.kt
@@ -0,0 +1,23 @@
+package com.joseph18.ifubaya.todoapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.joseph18.ifubaya.todoapp.R
+
+class EditTodoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_todo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
